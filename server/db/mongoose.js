b726807db58d80724710b29747ec9bac073cf818
kanmir <kiselev.dmitrii@mail.ru>
2018-05-25 14:54:15 +0300

    Revert "Add GET /todos route"
    This reverts commit 3e7986a09a422b55ba683d99f6bf6c6b4a6f6583.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,20 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbURI = 'mongodb://localhost:27017/TodoApp';
-
 mongoose.Promise = global.Promise;
-
-const connectWithRetry = function() {
-    return mongoose.connect(dbURI)
-        .then(() => {
-            console.log('Connected to database');
-        })
-        .catch((e) => {
-            console.error('Failed to connect to mongo on startup - retrying in 5 sec');
-            setTimeout(connectWithRetry, 5000);
-        });
-};
-
-connectWithRetry();
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
